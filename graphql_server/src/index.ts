b6f7ebc664e9942334e16134a9ffab57e2613bc0
p0ntan <pontus.akerberg@hotmail.com>
2024-03-23 09:51:16 +0100

    Change name on folder

---
+++ graphql_server/src/index.ts
@@ -0,0 +1,77 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import MariaDbHandler from "./MariaDbHandler";
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+    type Person {
+        id: ID
+        first_name: String
+        last_name: String
+        movies: [Movie]
+    }
+
+    type Manus {
+        id: ID
+        author: Person
+        year: Int
+    }
+
+    type Movie {
+        id: ID
+        title: String
+        rating: Float
+        actors: [Person]
+    }
+
+    type Category {
+        id: ID
+        type: String
+        movies(lt_rating: Float, gt_rating: Float): [Movie]
+    }
+
+    type Query {
+        persons: [Person]
+        person(id: ID!): Person
+        movies: [Movie]
+        movie (id: ID!): Movie
+        moviesByCategory(category: String!): [Movie]
+        categories: [Category]
+        category(id: ID!): Category
+    }
+`;
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        async persons() {
+            const handler = MariaDbHandler.getInstance();
+
+            return await handler.findAll('persons');
+        }
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
