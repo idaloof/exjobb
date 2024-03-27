bd16ce26051287cf954db41bdc2f84c3a2d7d17a
p0ntan <pontus.akerberg@hotmail.com>
2024-03-27 10:09:28 +0100

    Remove comments

--- graphql_joinmonster/src/index.ts
+++ graphql_joinmonster/src/index.ts
@@ -10,8 +10,6 @@ import joinMonster from 'join-monster';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
 // A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
     type Actor {
         id: ID
@@ -54,7 +52,6 @@ const typeDefs = `#graphql
 `;
 
 // Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
         actors (parent: undefined, args: {}, ctx: any, resolveInfo: any) {
@@ -233,17 +230,12 @@ joinMonsterAdapt(schema, {
     }
 });
 
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+// The ApolloServer uses the schema witch contains the typeDefs and resolvers
 const server = new ApolloServer({
     schema
 });
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
 });
