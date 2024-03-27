e32ad98524069e42cff5e3ee40705c8536a9d255
p0ntan <pontus.akerberg@hotmail.com>
2024-03-27 11:58:31 +0100

    Create index.ts, tsconfig and .gitignore to not upload database data

---
+++ graphql_neo4j/src/index.ts
@@ -0,0 +1,62 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+
+const typeDefs = `#graphql
+    type Actor {
+        id: ID
+        first_name: String
+        last_name: String
+        movies: [Movie] @relationship(type: "ACTED_IN", direction: IN)
+        manuscripts: [Manus]
+    }
+
+    type Manus {
+        id: ID
+        author: Actor
+        year: Int
+    }
+
+    type Character {
+        character: String
+        played_by: Actor
+    }
+
+    type Movie {
+        id: ID
+        title: String
+        rating: Float
+        characters: [Character]
+        categories: [String]
+    }
+
+    type Category {
+        type: String
+    }
+
+    type Query {
+        actors: [Actor]
+        actor(id: ID!): Actor
+        movies: [Movie]
+        movie (id: ID!): Movie
+        moviesByCategory(category: String!): [Movie]
+    }
+`;
+
+const driver = neo4j.driver(
+    "neo4j://localhost:7687",
+    neo4j.auth.basic("username", "password")
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+const server = new ApolloServer({
+    schema: await neoSchema.getSchema(),
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀 Server ready at ${url}`);
